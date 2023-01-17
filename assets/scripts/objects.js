9163aaec085fadee337624b489f989357739da50
jihwanAn <an0572@naver.com>
2023-01-17 19:10:22 +0900

    this 사용하여 title 대문자 출력

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -22,8 +22,9 @@ const renderMovies = (filter = "") => {
     const movieEl = document.createElement("li");
     const { info, ...otherProps } = movie;
     console.log(otherProps);
-    const { title: movieTitle } = info;
-    let text = title + "-";
+    // const { title: movieTitle } = info;
+    // const { getFormattedTitle } = movie;
+    let text = movie.getFormattedTitle() + "-"; // this가 movie 객체를 참조
     for (const key in info) {
       if (key !== "title") {
         text = text + `${key}: ${info[key]}`;
@@ -54,6 +55,9 @@ const addMovieHandler = () => {
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
+    getFormattedTitle: function () {
+      return this.info.title.toUpperCase();
+    },
   };
   movies.push(newMovie);
   renderMovies();
